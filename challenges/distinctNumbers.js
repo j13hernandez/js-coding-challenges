18fc54ae9976b2f8e9ad7081e1fe4021d53abc89
ptavarez16 <ptavarez16@yahoo.com>
2018-08-02 21:00:05 -0400

    Add solution to distinctNumber challenge

--- challenges/distinctNumbers.js
+++ challenges/distinctNumbers.js
@@ -5,7 +5,13 @@ number. 2 of the 3 parameters will always be the same.
 
 
 let distNum = function (num1, num2, num3) {
-  // Answer here
+  if (num1 == num2) {
+    return num3
+  } else if (num1 == num3) {
+    return num2
+  } else {
+    return num1
+  }
 }
 
 module.exports = distNum
